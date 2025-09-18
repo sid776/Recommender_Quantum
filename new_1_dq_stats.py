6fffd485b6239d2f44d4fc13bf8f2edbdf58e7a6
sid776 <119440598+sid776@users.noreply.github.com>
2025-09-18 11:58:42 -0400

    Create new_1_dq_stats.py

---
+++ new_1_dq_stats.py
@@ -0,0 +1,15 @@
+from typing import Any, Dict, List, Optional
+from datetime import date
+from ninja import Router
+
+from objects.dq_reports import DQReports
+
+router = Router(tags=["DQ Reports"])
+
+@router.get("/dq/combined", response=List[Dict[str, Any]])
+def dq_combined(
+    request,
+    report_date: Optional[date] = None,
+    limit: int = 500,
+):
+    return DQReports.get_all(report_date=report_date, limit=limit)
